6187ab562a4d6c23dda873006f7b159c8fcd86db
Sean Krasovic <m193480@usna.edu>
2017-10-24 21:13:28 -0400

    Add files via upload
    Fixed version so only one question at a time appears

--- QuizForm.php
+++ QuizForm.php
@@ -6,26 +6,31 @@
 
   <body>
 
-    <?php if(!isset($_POST['num'])) {
-      ?>
-    <form method="POST" action="?">
-      How many questions do you want?<input type="number" name="num" max="10" min="1" placeholder="10"/>
-    </form>
-<?php } else {?>
-    <form method="POST" action="quiz.php">
-      <?php
-        for($i=1;$i<=$_POST['num'];$i++) {
-          $num = $i -1;
-          echo "Question $i: <input type='text' name = 'questions[$num]['question']' maxlength ='140' /><br />";
-          for($j=0;$j<4;$j++) {
-            $n = $j + 1;
-            echo "Answer $n: <input type='text' name = 'questions[$num]['options']' maxlength ='50' /><br />";
-          }
-        }
-       ?>
-    </form>
 
-<?php } ?>
+  <form method="POST" action="?">
+    <?php
+    session_start();
+    if(!isset($_SESSION['i'])) {
+      $_SESSION['i'] = 0;
+    }else {
+      $_SESSION['i'] = $_SESSION['i'] + 1;
+    }
+    $i = $_SESSION['i'] + 1;
+    if(!($_SESSION['i']>9)) {
+    echo "Question $i: <input type='text' name = 'questions[{$_SESSION['i']}]['question']' maxlength ='140' /><br />";
+    for($j=0;$j<4;$j++) {
+      $n = $j + 1;
+      echo "Answer $n: <input type='text' name = 'questions[{$_SESSION['i']}]['options']' maxlength ='50' /><br />";
+    }
+
+      echo "<input type='submit' value='Question Submit' /><br />";
+    }
+
+     ?>
+  </form>
+  <form method ="POST" action="Quiz.php">
+  <?php echo "<input type='submit' value='Final Submit' />"; ?>
+  </form>
 
   </body>
 </html>
