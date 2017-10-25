b9ec3465bbcc0509a69e31a2d5f4cd93d0dbfa9d
Sean Krasovic <m193480@usna.edu>
2017-10-25 10:45:50 -0400

    Add files via upload
    Bootstrap addition as well as fixed Quiz Form

--- QuizForm.php
+++ QuizForm.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Quiz Form</title>
 
-  </head>
+  <!-- Bootstrap -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
 
   <body>
 
 
-  <form method="POST" action="?">
+  <form method="POST" action="quiz.php">
     <?php
     session_start();
     if(!isset($_SESSION['i'])) {
@@ -17,13 +37,14 @@
     }
     $i = $_SESSION['i'] + 1;
     if(!($_SESSION['i']>9)) {
-    echo "Question $i: <input type='text' name = 'questions[{$_SESSION['i']}]['question']' maxlength ='140' /><br />";
+    echo "<div class = 'container'><table> <tr><td>";
+    echo "Question $i:</td><td><input type='text' name = 'questions[{$_SESSION['i']}]['question']' maxlength ='140' /></td></tr>";
     for($j=0;$j<4;$j++) {
       $n = $j + 1;
-      echo "Option $n: <input type='text' name = 'questions[{$_SESSION['i']}]['options']' maxlength ='50' /><br />";
+      echo "<tr><td>Option $n: </td><td><input type='text' name = 'questions[{$_SESSION['i']}]['options']' maxlength ='50' /></td></tr>";
     }
 
-      echo "<input type='submit' value='Question Submit' /><br />";
+      echo "<tr><td colspan = '2'><input type='submit' class='btn btn-default' value='Question Submit' /></td></tr></table></div>";
     }
 
      ?>
@@ -32,11 +53,13 @@
   </form>
   <form method ="POST" action="quiz.php">
   <?php
+  echo "<div class = 'container'><table>";
   for($j=0;$j<4;$j++) {
     $n = $j + 1;
-    echo "Final Answer $n: <input type='text' name = 'answers[]' maxlength ='50' /><br />";
+    echo "<tr><td>Final Answer $n: </td><td><input type='text' name = 'answers[]' maxlength ='50' /></td></tr>";
   }
-  echo "<input type='submit' value='Final Submit' />"; ?>
+  echo "<input type='hidden' value = '$_POST' />";
+  echo "<tr><td><input type='submit' class='btn btn-default' value='Final Submit' /></td></tr></table></div>"; ?>
   </form>
 
   </body>
