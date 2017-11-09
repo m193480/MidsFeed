ae369254821fa33203de4b4ea08d9b4d08cf7cff
JR <m195466@usna.edu>
2017-11-08 23:54:02 -0500

    bring back generalized navbar
    blanket navbar for uniformity / visual appeal.

---
+++ navbar.php
@@ -0,0 +1,38 @@
+<?php
+echo   "
+  <!-- Bootstrap core CSS -->
+  <link href='vendor/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+
+  <!-- Custom styles for this template -->
+  <!--<link href='css/1-col-portfolio.css' rel='stylesheet'>-->
+<nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
+      <div class='container'>
+        <a class='navbar-brand' href='index.html'>MIDSFEED!</a>
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class='collapse navbar-collapse' id='navbarResponsive'>
+          <ul class='navbar-nav ml-auto'>
+            <li class='nav-item active'>
+              <a class='nav-link' href='index.html'>Home
+                <span class='sr-only'>(current)</span>
+              </a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='quiz.php'>Take A Quiz</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='QuizForm.php'>Create A Quiz</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='signIn.php'>Sign In</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link' href='signUp.php'>Sign Up</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </nav>";
+?>
