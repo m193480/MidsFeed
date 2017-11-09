44f91c1c8131b2d0cb1be79a8c007a2dc992313b
JR <m195466@usna.edu>
2017-11-08 23:36:01 -0500

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,19 +1,14 @@
 <html>
 
 <?php
-
-  //require_once("../../error.inc.php");
-  require_once("navbar.php");
+require_once("navbar.php");
 ?>
 
 <?php
-
-
 /*
 
 
-Current functionality:  Read in any CSV file and create a variable, $data, that holds all of the quizzes in the final
-in a logical manner. After the conclusion of all quizzes results are displayed.  Currently results are arbitrary.
+Current functionality: Read a given quiz from a file and write to the current user's .csv file storing quiz results.
 
 
 
@@ -23,103 +18,125 @@ in a logical manner. After the conclusion of all quizzes results are displayed. 
  * containing the name, answers, questions, and options for each user created quiz.
  */
 function readCSV($filename)
-  {
-    if($fp = fopen("$filename", 'r'))
-      {
+{
+    if ($fp = fopen("$filename", 'r')) {
         $questionNumber = 0;
-        $linenumber = 0;
-        while($line = fgetcsv($fp, 0, "|"))
-          {
-            if($linenumber == 0)
-              {
+        $linenumber     = 0;
+        while ($line = fgetcsv($fp, 0, "|")) {
+            if ($linenumber == 0) {
                 $data["quizzes"]["name"] = $line[0];
-                $j = 1;
-                for($i = 0; $i < 4; $i++)
-                {
-                  $data["quizzes"]["answers"][$i] = $line[$j];
-                  $j++;
-                }
-              }
-              else {
-                $len = sizeof($line);
-                $j = 0;
-                while($j < $len)
-                {
-                  $data["quizzes"]["questions"][$questionNumber]["question"] = $line[$j];
-                  $j++;
-                  for($i = 0; $i < 4; $i++)
-                  {
-                    $data["quizzes"]["questions"][$questionNumber]["options"][$i] = $line[$j];
+                $data["quizzes"]["desc"] = $line[1];
+                $j                       = 2;
+                for ($i = 0; $i < 4; $i++) {
+                    $data["quizzes"]["answers"][$i] = $line[$j];
                     $j++;
-                  }
-                  $questionNumber++;
                 }
-              }
-              $linenumber++;
-          }
-      }
-      else
-      {
+            } else {
+                $len = sizeof($line);
+                $j   = 0;
+                while ($j < $len) {
+                    $data["quizzes"]["questions"][$questionNumber]["question"] = $line[$j];
+                    $j++;
+                    for ($i = 0; $i < 4; $i++) {
+                        $data["quizzes"]["questions"][$questionNumber]["options"][$i] = $line[$j];
+                        $j++;
+                    }
+                    $questionNumber++;
+                }
+            }
+            $linenumber++;
+        }
+    } else {
         echo "invalid input file: Blame Sean Krasovic.";
-      }
-      return $data;
-  }
-  session_start();
-  if(isset($_POST['restart']))
-  {
+        echo "<div class='form-group'><form method='POST' action='?'>
+    <label for='newquiz'>New Quiz:</label>
+    <input type='submit' name='newquiz' value='Enter Quiz Name'>
+  </form></div>";
+        exit(1);
+    }
+    return $data;
+}
+session_start();
+if (isset($_POST['unsset'])) {
+    $saveValue = $_SESSION['FILE'];
     session_unset();
-  }
-  ?>
+    $_SESSION['FILE'] = $saveValue;
+}
+if (isset($_POST['quizid'])) {
+    $_SESSION['FILE'] = $_POST['quizid'];
+}
+if (isset($_POST['newquiz'])) {
+    session_unset();
+}
+?>
 
 <?php
-  if(!isset($_SESSION["data"]))
-  {
-    $_SESSION["data"] = readCSV("Sean Krasovic Quiz.csv");
+if (!isset($_SESSION["FILE"])) {
+    echo "<form method='POST' action='?'>";
+    echo "<div class='form-group'>
+      <label for='quiz'>Enter name of quiz:</label>";
+    echo "<input type='text' class='form-control' name='quizid' id='quiz'></div>";
+    echo "<input type=submit name='FILE' value='Take Quiz!'>";
+    echo "</form>";
+} else if (isset($_SESSION["FILE"]) && !isset($_SESSION["questionProgress"])) {
+    $_SESSION["data"]             = readCSV($_SESSION["FILE"]);
     $_SESSION["questionProgress"] = 0;
-  }
-  if($_SESSION["questionProgress"] >= sizeof($_SESSION["data"]["quizzes"]["questions"]))
-  {
-    echo "Based on your answers, you are a gorilla.";
-  }
-  else
-  {
+}
+if (isset($_SESSION["questionProgress"]) && $_SESSION["questionProgress"] >= sizeof($_SESSION["data"]["quizzes"]["questions"])) {
+    echo "<div class='jumbotron'>";
+    echo "<h2 class='display-3'>Based on your answers, you are a gorilla.</h2>";
+    echo "</div>";
+    file_put_contents($_SESSION['FILE'], "Sean Krasovic, dude is a gorilla", FILE_APPEND | LOCK_EX);
+} else {
     $quizData = $_SESSION["data"]["quizzes"];
-  }
-
-  ?>
+}
+?>
 
   <form method="POST" action="?">
 
   <?php
-  if(isset($quizData))
-  {
+if (isset($quizData)) {
+    echo "<div class='text-center'>";
+    echo "<div class='jumbotron'><h1 class='display-3'>";
     echo $quizData["name"];
+    echo "</h1>";
+    echo "<h2 class='display-2'>";
+    echo $quizData['desc'];
+    echo "</h2> <hr class='my-4'";
     $questionNumber = $_SESSION["questionProgress"];
-    echo "<br>";
+    echo "<p class='lead'>";
     echo $quizData["questions"][$questionNumber]["question"];
-    echo "<br>";
+    echo "</p><p class='lead'>";
     echo "Here are your options:";
-    echo "<br>";
-    //echo "<select name='{$quizData["questions"][$questionNumber]["question"]}'>";
-    foreach($quizData["questions"][$questionNumber]["options"] as $key => $option)
-    {
-      echo "<input type='radio' name='{$quizData["questions"][$questionNumber]["question"]}' value='$option'> $option";
-      echo "<br>";
+    echo "</p>";
+    foreach ($quizData["questions"][$questionNumber]["options"] as $key => $option) {
+        echo "<input type='radio' name='{$quizData["questions"][$questionNumber]["question"]}' value='$option'> $option";
+        echo "<br>";
     }
-    echo "</select>";
+    echo "</select></div>";
     echo "<input type=submit value='Submit Answer'>";
-  echo "</form>";
-  $_SESSION["questionProgress"]++;
-  }
-  ?>
-  <form method="POST" action="?">
-    <label for="unsset">Restart Quiz Progress:</label>
-    <input type="submit" name="restart" value="RESTART">
-  </form>
+    echo "</form></div>";
+    $_SESSION["questionProgress"]++;
+}
 
-<?php /*
-    echo "<pre>";
-    print_r($_SESSION["data"]);
-    echo "</pre>"; */
+if (isset($_SESSION['FILE'])) {
+    echo "<div class='form-group'>
+  <form method='POST' action='?'>
+    <label for='unsset'>Restart Quiz Progress:</label>
+    <input type='submit' name='unsset' value='RESTART'>
+  </form></div>";
+    echo "<div class='form-group'><form method='POST' action='?'>
+    <label for='newquiz'>New Quiz:</label>
+    <input type='submit' name='newquiz' value='Enter Quiz Name'>
+  </form></div>";
+}
+
+?>
+
+<?php
+/*
+echo "<pre>";
+print_r($_SESSION["data"]);
+echo "</pre>"; */
 ?>
 </html>
